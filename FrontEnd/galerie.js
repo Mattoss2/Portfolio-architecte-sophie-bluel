e681fad28f8168232b82bcf8a6dda3cbfb801366
Mattoss <m4ttoss1005@gmail.com>
2023-05-31 21:42:12 +0200

    commit clone

--- FrontEnd/galerie.js
+++ FrontEnd/galerie.js
@@ -13,40 +13,58 @@ function addWork(data) {
 
 
 	data.forEach(item => {
+		
 		const figure = document.createElement("figure");
 		const img = document.createElement("img");
 		const titre = document.createElement("figcaption");
 
 		img.src = item.imageUrl;
 		titre.innerHTML = item.title;
-		
+
 		figure.appendChild(img);
 		figure.appendChild(titre);
 		gallery.appendChild(figure);
-		 
-  
-	
 
-	
+
+
+	});
+};
+
+
+
+async function logCategoriesData() {
+	const response = await fetch("http://localhost:5678/api/categories");
+	const jsonData = await response.json();
+	console.log(jsonData);
+	addFilters(jsonData);
+	console.log(addFilters)
+};
+
+logCategoriesData();
+function addFilters(Data) {
+
+	const categoryFilter = document.getElementById("category-filter");
+
+	Data.forEach(item => {
+
+		const button = document.createElement("button")
+
+		button.value = item.id;
+		button.textContent = item.name;
+		categoryFilter.appendChild(button);
+		
+
+		
+			button.addEventListener("click", () => {
+				const category = button.value;
+			const filteredWorks= Data.filter(work=> work.category=== category);
+			button.value =item.id;
+			
+			addWork(filteredWorks);
+
+			
+				console.log(category);
+			})
 		})
+
 	}
-
-
-	const categoryButtons = document.querySelectorAll(".category-button");
-	categoryButtons.forEach(button => {
-		button.addEventListener("click", () => {
-		  const category = button.dataset.category;
-	  console.log(category);
-		})
-	  })
-
-	  async function logJSONData() {
-		const response = await fetch("http://localhost:5678/api/categories");
-		const jsonData = await response.json();
-		console.log(jsonData);
-		addWork(jsonData);
-		console.log(addWork)
-	  };
-
-	
-;
